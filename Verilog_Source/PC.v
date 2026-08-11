f0bcf2d33dd867fd3d6cc3460352ad185eafaf02
Rajendra Naik <rajjnaik1910@gmail.com>
2026-08-12 00:22:34 +0530

    Implement program counter (PC) module in Verilog

--- Verilog_Source/PC.v
+++ Verilog_Source/PC.v
@@ -1,1 +1,20 @@
+`timescale 1ns / 1ps
 
+module PC (
+
+    input             clk,
+    input             reset,
+    input      [31:0] next_pc,
+    output reg [31:0] pc
+
+);
+
+always @(posedge clk or posedge reset)
+begin
+    if (reset)
+        pc <= 32'h00000000;
+    else
+        pc <= next_pc;
+end
+
+endmodule
